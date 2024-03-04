41754cb77a794156f483277ba7ecaef7ba32dc09
Faran Shaikh <faran.shaikh@iqvia.com>
2024-03-04 20:41:34 +0100

    🎉 fix: theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
+import { ReactNode } from 'react';
 import type { Metadata } from 'next';
 import { Quicksand } from 'next/font/google';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import './globals.css';
+import { cn } from '@/lib/utils';
 
 const font = Quicksand({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Next14 App',
+  title: 'My Next 14 App',
   description: 'App Template',
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning >
-      <body className={font.className}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
-  console.log(111, theme);
+
   return (
     <Button variant="outline" size="icon" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
