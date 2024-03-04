2d8c67993da83202049cf242c291db7d2d46af1f
Faran Shaikh <faran.shaikh@iqvia.com>
2024-03-04 20:26:33 +0100

    📝 feat: add theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata } from "next";
-import { Quicksand } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Quicksand } from 'next/font/google';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import './globals.css';
 
-const font = Quicksand({ subsets: ["latin"] });
+const font = Quicksand({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Next14 App",
-  description: "App Template",
+  title: 'Next14 App',
+  description: 'App Template',
 };
 
 export default function RootLayout({
@@ -15,8 +16,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={font.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning >
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          storageKey="swag-app-theme"
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
