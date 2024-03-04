6448b18903430412fadce4e7329ce11c3ae37ab8
Faran Shaikh <faran.shaikh@iqvia.com>
2024-03-04 22:15:00 +0100

    🎉 fix: show active route

--- components/navigation/nav-sidebar.tsx
+++ components/navigation/nav-sidebar.tsx
@@ -7,29 +7,28 @@ import ThemeToggle from '@/components/theme-toggle';
 const NavSidebar = () => {
   return (
     <nav className="space-y-4 flex flex-col font-semibold items-center h-full w-full bg-gray-100 dark:bg-[#1e1f22] p-4">
-      <button className="group flex items-center w-full gap-4">
-        <div className="flex h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
+      <button className="group relative flex items-center w-full gap-4">
+        <div className="absolute -left-4 bg-primary rounded-r-full transition-all w-[6px]  h-full" />
+        <div className="flex h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-primary">
           <Workflow className="group-hover:text-white transition" size={23} />
         </div>
-       Company Name
+        Company
       </button>
 
       <Separator className="h-[1px] bg-zinc-300 dark:bg-zinc-700 rounded-md !mt-4 mx-auto" />
       <ScrollArea className="flex flex-col gap-y-4 w-full">
-
-      <button className="group flex items-center w-full gap-4 mb-4">
-        <div className="flex h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
-          <Home className="group-hover:text-white transition" size={23} />
-        </div>
-       Company Name
-      </button>
-
+        <button className="group relative  flex items-center w-full gap-4 mb-4">
+          <div className="flex h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-primary">
+            <Home className="group-hover:text-white transition" size={23} />
+          </div>
+          Home
+        </button>
 
         {Array(3)
           .fill('-')
           .map((_, i) => (
             <button key={i} className="group flex items-center w-full gap-4 mb-4">
-              <div className="flex h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
+              <div className="flex h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-primary">
                 <Plus className="group-hover:text-white transition" size={25} />
               </div>
               Menu Item {i + 1}
