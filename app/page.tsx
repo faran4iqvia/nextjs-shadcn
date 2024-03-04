c78c58ef125d5a47ac5966215b3ee5f47d60d7c3
Faran Shaikh <faran.shaikh@iqvia.com>
2024-03-04 16:57:21 +0100

    🔒️ chore: test add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <main className="">
      <p>Hii</p>
+     <Button>adas</Button>
+     <Button variant={'destructive'}>adas</Button>
+     <Button variant={'secondary'}>adas</Button>
+     <Button variant={'ghost'}>adas</Button>
+     <Button variant={'link'}>adas</Button>
+     <Button variant={'outline'}>adas</Button>
+     <Button variant={'default'}>adas</Button>
     </main>
   );
 }
